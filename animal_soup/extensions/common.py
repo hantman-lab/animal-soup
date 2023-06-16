0642b5d0d7598199e1ec537f0ffaf7703b4cf32c
Caitlin Lewis <clewis7@email.unc.edu>
2023-06-16 09:15:13 -0400

    begin api refactor, better documentation, demo notebook

--- animal_soup/extensions/common.py
+++ animal_soup/extensions/common.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pathlib import Path
 import shutil
 import time
+import warnings
 
 
 @pd.api.extensions.register_dataframe_accessor("behavior")
@@ -17,8 +18,19 @@ class BehaviorDataFrameExtension:
             animal_id: str,
             session_id: Union[str, None] = None):
         """
-        Add item to dataframe, if session_id is None will add all available sessions for that animal.
+        Add item to dataframe. If `animal_id` already exists, will try to add any new videos to
+        `ethograms` dictionary for given session(s).
+
+        Parameters
+        ----------
+        animal_id: str
+            Animal to add to the dataframe.
+        session_id: str or None, default None
+            Session to be added for a given animal. If `None`, will attempt to add all sessions for
+            the given animal.
         """
+        # simple fix for now to supress PerformanceWarning when saving dataframe
+        warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
         if get_parent_raw_data_path() is None:
             raise ValueError(
@@ -37,86 +49,105 @@ class BehaviorDataFrameExtension:
                 f"animal_id path is not valid at: {animal_dir}"
             )
 
-
+        # session_id = None, get all sessions under that animal_id
         if session_id is None:
             # gets all sessions under an animal_id
             session_dirs = sorted(animal_dir.glob('*'))
             if len(session_dirs) == 0:
-                raise ValueError("no sessions found in this animal dir")
+                raise ValueError(f"no sessions found in this animal dir: {animal_dir}")
             for session_dir in session_dirs:
-                # check if already in dataframe # if so raise error
-                if len(self._df.index[
-                           (self._df['animal_id'] == animal_id) & (self._df['session_id'] == session_dir.stem)].tolist()) > 0:
-                    raise ValueError(
-                        f"item already exists with animal_id={animal_id} and session_id={session_dir.stem} \n"
-                        f"must remove item before trying to add"
+                # check if already in dataframe
+                # add new trials to ethograms if so
+                df_ix = self._df.index[
+                           (self._df['animal_id'] == animal_id) & (self._df['session_id'] == session_dir.stem)].tolist()
+                if len(df_ix) > 0:
+                    warnings.warn(
+                        f"\n Item already exists with animal_id={animal_id} and session_id={session_dir.stem}. \n"
+                        f"Will try to add any new videos. "
                     )
-                session_id = session_dir.stem
-                mat_path = session_dir.joinpath('jaaba.mat')
-                mat_path = self._df.paths.split(mat_path)[-1]
-                if os.path.exists(session_dir.joinpath('deg_labels.npy')):
-                    deg_path = session_dir.joinpath('deg_labels.npy')
-                    deg_path = self._df.paths.split(deg_path)[-1]
-                else:
-                    deg_path = None
+                    session_vids = sorted(session_dir.glob('*.avi'))
+                    session_vids = [vid_path.stem for vid_path in session_vids]
+
+                    for sv in session_vids:
+                        if sv not in self._df.loc[:, "ethograms"].loc[df_ix].keys():
+                            self._df.loc[:, "ethograms"].loc[df_ix][sv] = None
+                else: # want to add all sessions
+                    session_id = session_dir.stem
+                    session_vids = sorted(session_dir.glob('*.avi'))
+                    session_vids = [vid_path.stem for vid_path in session_vids]
+
+                    ethograms = dict()
+
+                    # add the keys for the available trial videos in a given session
+                    for sv in session_vids:
+                        ethograms[sv] = None
+
+                    s = pd.Series(
+                        {
+                            "animal_id": animal_id,
+                            "session_id": session_id,
+                            "ethograms": ethograms,
+                            "notes": None
+                        }
+                    )
+
+                    self._df.loc[self._df.index.size] = s
+
+                self._df.to_hdf(self._df.paths.get_df_path(), key='df')
+        else: # session_id is provided in args
+            # check if already in dataframe # if so raise error
+            if len(self._df.index[
+                       (self._df['animal_id'] == animal_id) & (self._df['session_id'] == session_id)].tolist()) > 0:
+                warnings.warn(
+                    f"\n Item already exists with animal_id={animal_id} and session_id{session_id}. \n"
+                    f"Will try to add any new trial videos."
+                )
+                session_dir = animal_dir.joinpath(session_id)
+
                 session_vids = sorted(session_dir.glob('*.avi'))
-                session_vids = [self._df.paths.split(vid_path)[-1] for vid_path in session_vids]
+                session_vids = [vid_path.stem for vid_path in session_vids]
+
+                for sv in session_vids:
+                    if sv not in self._df.loc[:, "ethograms"].loc[df_ix].keys():
+                        self._df.loc[:, "ethograms"].loc[df_ix][sv] = None
+
+            else:
+                session_dir = animal_dir.joinpath(session_id)
+                session_vids = sorted(session_dir.glob('*.avi'))
+                session_vids = [vid_path.stem for vid_path in session_vids]
+
+                ethograms = dict()
+
+                for sv in session_vids:
+                    ethograms[sv] = None
 
                 s = pd.Series(
                     {
                         "animal_id": animal_id,
                         "session_id": session_id,
-                        "mat_path": mat_path,
-                        "deg_path": deg_path,
-                        "session_vids": session_vids,
-                        "hand_training_trials": list(),
-                        "jabba_training_trials": list(),
+                        "ethograms": ethograms,
                         "notes": None
                     }
                 )
 
                 self._df.loc[self._df.index.size] = s
 
-                self._df.to_hdf(self._df.paths.get_df_path(), key='df')
-        else:
-            # check if already in dataframe # if so raise error
-            if len(self._df.index[
-                       (self._df['animal_id'] == animal_id) & (self._df['session_id'] == session_id)].tolist()) > 0:
-                raise ValueError(
-                    f"item already exists with animal_id={animal_id} and session_id{session_id} \n"
-                    f"must remove item before trying to add"
-                )
-            session_dir = animal_dir.joinpath(session_id)
-            mat_path = session_dir.joinpath('jaaba.mat')
-            mat_path = self._df.paths.split(mat_path)[-1]
-            if os.path.exists(session_dir.joinpath('deg_labels.npy')):
-                deg_path = session_dir.joinpath('deg_labels.npy')
-                deg_path = self._df.paths.split(deg_path)[-1]
-            else:
-                deg_path = None
-            session_vids = sorted(session_dir.glob('*.avi'))
-            session_vids = [self._df.paths.split(vid_path)[-1] for vid_path in session_vids]
-            s = pd.Series(
-                {
-                    "animal_id": animal_id,
-                    "session_id": session_id,
-                    "mat_path": mat_path,
-                    "deg_path": deg_path,
-                    "session_vids": session_vids,
-                    "hand_training_trials": list(),
-                    "jabba_training_trials": list(),
-                    "notes": None
-                }
-            )
-
-            self._df.loc[self._df.index.size] = s
-
             self._df.to_hdf(self._df.paths.get_df_path(), key='df')
 
     def save_to_disk(self, max_index_diff: int = 0):
         """
         Saves DataFrame to disk, copies to a backup before overwriting existing file.
+
+        Parameters
+        ----------
+        max_index_diff: int, default 0
+            Max difference in the number of indices between the current dataframe and the dataframe
+            stored on disk. If index difference exceeds max, will raise `IndexError`.
+
         """
+        # simple fix for now to supress PerformanceWarning when saving dataframe
+        warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+
         path: Path = self._df.paths.get_df_path()
 
         disk_df = load_df(path)
@@ -144,9 +175,24 @@ class BehaviorDataFrameExtension:
             raise IOError(f"Could not save dataframe to disk.")
 
     def remove_item(self, animal_id: str,  session_id: Union[str, None] = None):
-        """Remove item from dataframe."""
+        """
+        Remove item(s) from dataframe.
+
+        Parameters
+        ----------
+
+        animal_id: str
+            Animal to remove from dataframe.
+        session_id: str or None, default None
+            Session to remove from dataframe. If `None`, will remove all sessions for the provided
+            animal.
+        """
+        # simple fix for now to supress PerformanceWarning when saving dataframe
+        warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
         if session_id is None:
+            warnings.warn("No `session_id` provided, will remove all items for provided "
+                          f"animal_id: {animal_id}")
             # remove all items for animal
             index = self._df.index[self._df['animal_id'] == animal_id].tolist()
         else:
