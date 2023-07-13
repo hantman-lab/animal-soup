73896e5e0055d7d55df852bbd51d9d400408fb1e
Caitlin Lewis <clewis7@email.unc.edu>
2023-07-13 17:06:48 -0400

    basic docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,18 +3,23 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# from bs4 import BeautifulSoup
+# from typing import *
+# import animal_soup
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'animal-soup'
 copyright = '2023, Caitlin Lewis'
 author = 'Caitlin Lewis'
-release = '0.0.1a1'
+release = "0.0.1a1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.napoleon"]
+autodoc_typehints = "description"
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +29,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
+#html_theme_options = {"page_sidebar_items": ["class_page_toc"]}
+autoclass_content = "both"
+
 html_static_path = ['_static']
+
+autodoc_member_order = 'bysource'
