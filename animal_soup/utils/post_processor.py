f04e808ef4761d38c000dc0074fd6bfde03f9d62
Caitlin Lewis <clewis7@email.unc.edu>
2023-07-30 15:52:59 -0400

    add code comments

--- animal_soup/utils/post_processor.py
+++ animal_soup/utils/post_processor.py
@@ -1,23 +1,17 @@
+"""
+Post-processing utility functions.
+
+Adopted from: https://github.com/jbohnslav/deepethogram/blob/master/deepethogram/postprocessing.py
+"""
+
 import numpy as np
 
 
-def find_bout_indices(predictions_trace: np.ndarray,
-                      bout_length: int,
-                      positive: bool = True,
-                      eps: float = 1e-6) -> np.ndarray:
-    """
-
-    Parameters
-    ----------
-    predictions_trace
-    bout_length
-    positive
-    eps
-
-    Returns
-    -------
-
-    """
+def _find_bout_indices(predictions_trace: np.ndarray,
+                       bout_length: int,
+                       positive: bool = True,
+                       eps: float = 1e-6) -> np.ndarray:
+    """Utility function for finding where bouts of each behavior occur so they can be removed if too short."""
     # make a filter for convolution that will be 1 at that bout center
     center = np.ones(bout_length) / bout_length
     filt = np.concatenate([[-bout_length / 2], center, [-bout_length / 2]])
@@ -44,16 +38,26 @@ def min_bout_post_process(
         min_bout_length: int = 1
 ):
     """
+    Post-processing technique to remove detected behaviors that occur in bouts less than the
+    specified length.
 
     Parameters
     ----------
-    thresholds
-    predictions
-    min_bout_length
+    thresholds: np.ndarray
+        A 1D array representing the thresholds required to label a single time point as a certain behavior.
+        For now, using default thresholds for the Hantman reach-to-grab task but should go back and
+        implement calculating the thresholds during training of the feature extractor.
+    predictions: dict
+        Dictionary containing the probabilities and logits from sequence inference.
+    min_bout_length: int, default 1
+        The minimum default bout length a behavior must occur across consecutive time-points to be
+        classified as occurring.
 
     Returns
     -------
-
+    final_preds: np.ndarray
+        An array of classified behaviors in the shape [# behaviors, # time points]. The "background"
+        behavior used in training/inference is removed.
     """
     predictions = (predictions["probabilities"] > thresholds).astype(int)
 
@@ -63,10 +67,10 @@ def min_bout_post_process(
         predictions_trace = predictions[:, k]
         for bout_len in range(1, min_bout_length + 1):
             # first, remove "false negatives", like filling in gaps in true behavior bouts
-            short_neg_indices = find_bout_indices(predictions_trace, bout_len, positive=False)
+            short_neg_indices = _find_bout_indices(predictions_trace, bout_len, positive=False)
             predictions_trace[short_neg_indices] = 1
             # then remove "false positives", very short "1" bouts
-            short_pos_indices = find_bout_indices(predictions_trace, bout_len)
+            short_pos_indices = _find_bout_indices(predictions_trace, bout_len)
             predictions_trace[short_pos_indices] = 0
         predictions[:, k] = predictions_trace
 
